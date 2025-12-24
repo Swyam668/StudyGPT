b737e08b44c1574e6f3c67a36b302d4e7078fbd0
Swyam668 <swyamv04@gmail.com>
2025-12-24 17:15:00 +0530

    Forming organised folder structure and mounting raw login, register components.

--- frontend/StudyGPT/src/App.jsx
+++ frontend/StudyGPT/src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/Auth/LoginPage';
+import RegisterPage from './pages/Auth/RegisterPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
-  return(
-    <div>
-      App
-    </div>
+  const isAuthenticated = false
+  const loading = false
+
+  if(loading){
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/"
+        // replace is used for replacing 'the page we went from' with 'the page we went to' in browser histoy, without stacking them up
+        element={isAuthenticated ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />}></Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        
+
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   )
 }
 
